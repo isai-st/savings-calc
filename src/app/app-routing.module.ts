df1d7b3ef758d137a05b3e9174fee8be501782bf
isai <isaiorellana.dev@gmail.com>
2024-01-09 11:00:58 -0600

    Refactor to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { IsaiComponent } from './isai/isai.component';
-import { PeterComponent } from './peter/peter.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'isai', component: IsaiComponent },
-  { path: 'peter', component: PeterComponent },
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
